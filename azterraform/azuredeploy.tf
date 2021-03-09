9ce1bcbeebaa623f8914ce6cc1c4152c00ebe608
tonytregcdw <54070632+tonytregcdw@users.noreply.github.com>
2021-03-09 15:36:51 +0000

    Update azuredeploy.tf

--- azterraform/azuredeploy.tf
+++ azterraform/azuredeploy.tf
@@ -35,6 +35,15 @@ resource "azurerm_subnet" "region1-vnet1-snet1" {
   virtual_network_name = azurerm_virtual_network.region1-vnet1-hub1.name
   address_prefixes     = [var.region1-vnet1-snet1-range]
 }
+#VNET Peering
+resource "azurerm_virtual_network_peering" "peer1" {
+  name                      = "region1-vnet1-to-infra-vnet1"
+  resource_group_name       = azurerm_resource_group.rg1.name
+  virtual_network_name      = azurerm_virtual_network.region1-vnet1-hub1.name
+  remote_virtual_network_id = azurerm_virtual_network.TT_Infrastructure_RG-vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
 #resource "azurerm_subnet" "AzureBastionSubnet" {
 #  name                 = "AzureBastionSubnet"
 #  resource_group_name  = azurerm_resource_group.rg1.name
